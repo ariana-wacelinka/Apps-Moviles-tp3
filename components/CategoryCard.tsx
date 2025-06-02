33c0800fd2abca906307cca2a5305b3b651b9fe2
NachoL24 <ignaciolances24@gmail.com>
2025-06-01 21:11:35 -0300

    card de categoria

---
+++ components/CategoryCard.tsx
@@ -0,0 +1,108 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Category } from '../types/categories';
+
+interface CategoryCardProps {
+  category: Category;
+  onPress?: (category: Category) => void;
+}
+
+const CategoryCard: React.FC<CategoryCardProps> = ({ category, onPress }) => {
+  return (
+    <TouchableOpacity 
+      style={styles.cardContainer} 
+      onPress={() => onPress && onPress(category)}
+      activeOpacity={0.7}
+    >
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>{category.strCategory}</Text>
+      </View>
+      <View style={styles.imageWrapper}>
+        <Image source={{ uri: category.strCategoryThumb }} style={styles.image} />
+        <LinearGradient
+          colors={['transparent', 'transparent', 'rgba(240,240,240,0)', 'rgba(240,240,240,0.3)', '#f0f0f0']}
+          locations={[0, 0.3, 0.6, 0.8, 1]}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 0 }}
+          style={styles.fadeGradient}
+          pointerEvents="none"
+        />
+        <LinearGradient
+          colors={['transparent', 'transparent', 'rgba(240,240,240,0)', 'rgba(240,240,240,0.5)', '#f0f0f0']}
+          locations={[0, 0.3, 0.6, 0.8, 1]}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 0, y: 0 }}
+          style={styles.fadeGradient}
+          pointerEvents="none"
+        />
+        <LinearGradient
+          colors={['transparent', 'transparent', 'rgba(240,240,240,0)', 'rgba(240,240,240,0.3)', '#f0f0f0']}
+          locations={[0, 0.3, 0.6, 0.8, 1]}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 0 }}
+          style={styles.fadeGradientrotated}
+          pointerEvents="none"
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    marginVertical: 6,
+    paddingLeft: 10,
+    paddingTop: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  imageWrapper: {
+    width: 80,
+    height: 90,
+    overflow: 'hidden',
+    marginRight: 0,
+    position: 'relative',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    transform: [{ scale: 1.2}, { translateX: 5 }],
+  },
+  fadeGradient: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  fadeGradientrotated: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    transform: [{ rotate: '20deg' }, { scale: 1.2}],
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default CategoryCard;
